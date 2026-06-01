28f354dfc9be057abc815e1326e585f0a099dd9a
DavidHunterScot <david.hunter@echoverse.cc>
2026-06-01 18:06:54 +0100

    Updated bookmark link in footer.

--- private/views/footer.php
+++ private/views/footer.php
@@ -4,7 +4,7 @@
         
         <footer class="w3-topbar w3-border-gray background-alt">
             <div class="container w3-small">
-                <p>Bookmark this URL to Network Tools: <a href="https://weburl.net/networktools" target="_blank">weburl.net/networktools</a></p>
+                <p>Bookmark this URL: <a href="https://networktools.dah5.me.uk" target="_blank">networktools.dah5.me.uk</a></p>
             </div>
         </footer>
     </body>
